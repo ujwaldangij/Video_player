aede4f3fab368193eb51ea1c61680a44bd31103d
Ujwal Dangi <43333055+ujwaldangij@users.noreply.github.com>
2023-10-09 11:56:17 +0530

    Update FileDownloadTest.php

--- app/Http/Controllers/FileDownloadTest.php
+++ app/Http/Controllers/FileDownloadTest.php
@@ -1,4 +1,27 @@
-var inputField = document.querySelector("#ContentPlaceHolder1_ImageButton12");
-                if(inputField) {
-                    inputField.click();
-                }
+In Laravel Dusk, you can simulate running your tests in "incognito" mode by using the `browsingAs` method with a custom browser instance that has specific settings. Here's how you can do it:
+
+```php
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class YourDuskTest extends DuskTestCase
+{
+    public function testInIncognitoMode()
+    {
+        $this->browse(function (Browser $browser) {
+            // Use a custom browser instance with incognito settings
+            $browser->driver->capabilities->setCapability('chromeOptions', [
+                'args' => ['--incognito'],
+            ]);
+
+            // Your test code here
+            $browser->visit('/')
+                    ->assertSee('Welcome to Your Website');
+        });
+    }
+}
+```
+
+In this example, we set the `'--incognito'` command-line argument to simulate running the test in incognito mode. Replace `'/welcome'` with the URL you want to test.
+
+Keep in mind that this method only simulates incognito mode for the Dusk test itself. It won't affect your regular browser sessions.
