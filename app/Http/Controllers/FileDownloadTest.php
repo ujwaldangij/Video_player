0f0bae722b1b05e942924c05c46e1d4fc46fe5c1
Ujwal Dangi <43333055+ujwaldangij@users.noreply.github.com>
2023-10-09 14:10:32 +0530

    Update FileDownloadTest.php

--- app/Http/Controllers/FileDownloadTest.php
+++ app/Http/Controllers/FileDownloadTest.php
@@ -1,38 +1,51 @@
-In Laravel Dusk, you can automate the interaction with a download prompt by setting Chrome options using the `using` method on the `DuskTestCase` class. Specifically, you can configure Chrome to automatically download files without displaying a download prompt. Here's how you can do it:
+To ensure that Laravel Dusk runs in incognito mode and doesn't prompt to save files every time, you need to configure your ChromeOptions when setting up your DuskTestCase.php. Specifically, you can use the `--no-sandbox` flag and specify a custom download directory to avoid the prompts for file downloads. Here's how you can do it:
 
-1. Open your `DuskTestCase.php` file (usually located in the `tests/Dusk` directory).
+In your `DuskTestCase.php` file, configure the Chrome options like this:
 
-2. Import the necessary classes at the top of the file:
+```php
+use Laravel\Dusk\TestCase as BaseTestCase;
+use Facebook\WebDriver\Chrome\ChromeOptions;
 
-   ```php
-   use Laravel\Dusk\TestCase as BaseTestCase;
-   use Facebook\WebDriver\Chrome\ChromeOptions;
-   ```
+class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
 
-3. In the `driver` method of your `DuskTestCase` class, configure Chrome to automatically download files:
+    protected function driver()
+    {
+        $options = (new ChromeOptions())->addArguments([
+            '--disable-gpu',
+            '--headless',
+            '--no-sandbox', // Add this flag
+            '--disable-extensions',
+            '--disable-dev-shm-usage',
+            '--disable-popup-blocking',
+            '--disable-infobars',
+            '--disable-notifications',
+            '--disable-software-rasterizer',
+            '--disable-web-security', // Add this flag to disable web security (incognito-like mode)
+            '--user-data-dir=' . storage_path('app/chrome'), // Use a custom user data directory to avoid prompts
+            '--disk-cache-dir=' . storage_path('app/chrome/cache'), // Use a custom cache directory
+        ]);
 
-   ```php
-   protected function driver()
-   {
-       $options = (new ChromeOptions())->addArguments([
-           '--disable-extensions',  // Disable extensions (e.g., Chrome extensions that might affect downloads).
-           '--disable-notifications', // Disable notifications (e.g., download notifications).
-           '--headless', // Run Chrome in headless mode (optional but can be useful for automated tests).
-           '--disable-gpu', // Disable GPU (optional but can improve performance).
-           '--no-sandbox', // Disable sandboxing (useful in some environments).
-           '--disable-software-rasterizer', // Disable software rasterizer (optional but can improve performance).
-           '--safebrowsing-disable-download-protection', // Disable download protection (which can show download prompts).
-       ]);
+        return RemoteWebDriver::create(
+            'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY,
+                $options
+            )
+        );
+    }
+}
+```
 
-       return RemoteWebDriver::create(
-           'http://localhost:9515',
-           DesiredCapabilities::chrome()->setCapability(ChromeOptions::CAPABILITY, $options)
-       );
-   }
-   ```
+In the code above:
 
-   The key argument here is `--safebrowsing-disable-download-protection`, which disables download protection that can show download prompts.
+- We add the `--no-sandbox` flag to Chrome options to disable the sandbox, which can sometimes cause issues in headless mode.
 
-4. Save the changes to your `DuskTestCase.php` file.
+- We use `--disable-web-security` to disable web security, which is akin to incognito mode and can prevent certain prompts.
 
-With these settings, Chrome will be configured to automatically download files without displaying download prompts during your Laravel Dusk tests. Make sure you're aware of the potential security implications when disabling download protection and use this configuration responsibly, ideally in a controlled testing environment.
+- We specify custom directories for user data and cache to avoid any permission issues and to ensure files are downloaded to a location you control.
+
+Please replace the paths (`storage_path('app/chrome')` and `storage_path('app/chrome/cache')`) with the actual paths you want to use for your user data and cache directories.
+
+With these configurations, Laravel Dusk should run in a more incognito-like mode, and you should be able to download files without being prompted to save them every time.
