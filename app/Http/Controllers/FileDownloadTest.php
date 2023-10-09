8b0160580a101065cbbda1776588bfa7e86dacb1
Ujwal Dangi <43333055+ujwaldangij@users.noreply.github.com>
2023-10-09 13:55:22 +0530

    Update FileDownloadTest.php

--- app/Http/Controllers/FileDownloadTest.php
+++ app/Http/Controllers/FileDownloadTest.php
@@ -1,27 +1,38 @@
-In Laravel Dusk, you can simulate running your tests in "incognito" mode by using the `browsingAs` method with a custom browser instance that has specific settings. Here's how you can do it:
+In Laravel Dusk, you can automate the interaction with a download prompt by setting Chrome options using the `using` method on the `DuskTestCase` class. Specifically, you can configure Chrome to automatically download files without displaying a download prompt. Here's how you can do it:
 
-```php
-use Laravel\Dusk\Browser;
-use Tests\DuskTestCase;
+1. Open your `DuskTestCase.php` file (usually located in the `tests/Dusk` directory).
 
-class YourDuskTest extends DuskTestCase
-{
-    public function testInIncognitoMode()
-    {
-        $this->browse(function (Browser $browser) {
-            // Use a custom browser instance with incognito settings
-            $browser->driver->capabilities->setCapability('chromeOptions', [
-                'args' => ['--incognito'],
-            ]);
+2. Import the necessary classes at the top of the file:
 
-            // Your test code here
-            $browser->visit('/')
-                    ->assertSee('Welcome to Your Website');
-        });
-    }
-}
-```
+   ```php
+   use Laravel\Dusk\TestCase as BaseTestCase;
+   use Facebook\WebDriver\Chrome\ChromeOptions;
+   ```
 
-In this example, we set the `'--incognito'` command-line argument to simulate running the test in incognito mode. Replace `'/welcome'` with the URL you want to test.
+3. In the `driver` method of your `DuskTestCase` class, configure Chrome to automatically download files:
 
-Keep in mind that this method only simulates incognito mode for the Dusk test itself. It won't affect your regular browser sessions.
+   ```php
+   protected function driver()
+   {
+       $options = (new ChromeOptions())->addArguments([
+           '--disable-extensions',  // Disable extensions (e.g., Chrome extensions that might affect downloads).
+           '--disable-notifications', // Disable notifications (e.g., download notifications).
+           '--headless', // Run Chrome in headless mode (optional but can be useful for automated tests).
+           '--disable-gpu', // Disable GPU (optional but can improve performance).
+           '--no-sandbox', // Disable sandboxing (useful in some environments).
+           '--disable-software-rasterizer', // Disable software rasterizer (optional but can improve performance).
+           '--safebrowsing-disable-download-protection', // Disable download protection (which can show download prompts).
+       ]);
+
+       return RemoteWebDriver::create(
+           'http://localhost:9515',
+           DesiredCapabilities::chrome()->setCapability(ChromeOptions::CAPABILITY, $options)
+       );
+   }
+   ```
+
+   The key argument here is `--safebrowsing-disable-download-protection`, which disables download protection that can show download prompts.
+
+4. Save the changes to your `DuskTestCase.php` file.
+
+With these settings, Chrome will be configured to automatically download files without displaying download prompts during your Laravel Dusk tests. Make sure you're aware of the potential security implications when disabling download protection and use this configuration responsibly, ideally in a controlled testing environment.
