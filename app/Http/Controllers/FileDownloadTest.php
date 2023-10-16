dd6bb696ea6adf312a9c4ec1bc2698332fcfd0cc
Ujwal Dangi <43333055+ujwaldangij@users.noreply.github.com>
2023-10-16 11:51:34 +0530

    Update FileDownloadTest.php

--- app/Http/Controllers/FileDownloadTest.php
+++ app/Http/Controllers/FileDownloadTest.php
@@ -1,12 +1,19 @@
-In Laravel Dusk, you can check if a specific text is present on a page using the `assertSee` method. If you want to visually mark the text, you can do so by using JavaScript to highlight or style the element containing the text. Here's an example of how to achieve this:
+I apologize for the confusion. It seems there was an issue with the `:contains` selector, which is not a standard CSS selector. To highlight and check for the presence of specific text, you can use JavaScript to achieve this. Here's an example that should work:
 
 ```php
 $year = date('Y'); // Get the current year
 $textToFind = "$year"; // Text to find on the page
 
 $browser->visit('/your-page-url') // Navigate to your page
-    ->script("document.querySelectorAll('*:contains(\"$textToFind\")').forEach(function(element) { element.style.backgroundColor = 'yellow'; });") // Highlight the text
-    ->assertSee($textToFind); // Check if the text is present and highlighted
+    ->script("var elements = document.querySelectorAll('*'); 
+              for (var i = 0; i < elements.length; i++) { 
+                  var element = elements[i];
+                  if (element.textContent.includes('$textToFind')) {
+                      element.style.backgroundColor = 'yellow';
+                      element.style.color = 'black';
+                  }
+              }") // Highlight the text
+    ->assertSee($textToFind); // Check if the text is present
 ```
 
-In the above code, we first navigate to the page and then use JavaScript to highlight all elements containing the text (in this case, the current year). We set the background color to yellow, but you can adjust the styling as needed. After highlighting the text, we use `assertSee` to verify that the text is present on the page. If it's found and highlighted, the assertion will pass.
+In this code, we use JavaScript to iterate through all elements on the page and check if their `textContent` contains the text you want to find. If found, we apply the highlight style, and then we use `assertSee` to ensure the text is present. Make sure to replace `"/your-page-url"` with the actual URL of the page you're testing.
