98a78843e07072b1f175c28babd30aff662fba2f
Ujwal Dangi <43333055+ujwaldangij@users.noreply.github.com>
2023-10-16 11:41:32 +0530

    Update FileDownloadTest.php

--- app/Http/Controllers/FileDownloadTest.php
+++ app/Http/Controllers/FileDownloadTest.php
@@ -1,21 +1,12 @@
-If you're encountering an "illegal selector was specified" error in Laravel Dusk, it's likely due to a problem with the CSS selector you're using. To resolve this issue, consider the following steps:
-
-1. **Review the Selector:** Double-check the selector you're using to locate the element. Ensure that it accurately matches the element you want to interact with. Pay attention to the structure and attributes of the HTML.
-
-2. **Valid CSS Selector:** Make sure you're using a valid CSS selector. Common CSS selectors include class selectors (e.g., `.classname`), ID selectors (e.g., `#idname`), element selectors (e.g., `div`, `span`), and attribute selectors (e.g., `[data-attribute="value"]`).
-
-3. **Use XPath:** If CSS selectors are not sufficient, consider using XPath expressions to locate the element. XPath provides more powerful ways to select elements in the DOM.
-
-4. **Check for Dynamic Content:** If your page contains dynamic content that may not be available immediately, ensure your Dusk test waits for the content to load. Use the `waitFor` method or other waiting techniques to handle dynamic content.
-
-5. **Debugging:** Add debugging statements to your Dusk test to print out information about the element and the HTML source of the page. This can help you troubleshoot selector issues.
-
-Here's an example using XPath to locate the text "2023" and then click a `span` tag:
+In Laravel Dusk, you can check if a specific text is present on a page using the `assertSee` method. If you want to visually mark the text, you can do so by using JavaScript to highlight or style the element containing the text. Here's an example of how to achieve this:
 
 ```php
-$textToFind = '2023';
+$year = date('Y'); // Get the current year
+$textToFind = "$year"; // Text to find on the page
 
-$browser->driver->findElement(WebDriverBy::xpath("//*[text()='$textToFind']/following-sibling::span"))->click();
+$browser->visit('/your-page-url') // Navigate to your page
+    ->script("document.querySelectorAll('*:contains(\"$textToFind\")').forEach(function(element) { element.style.backgroundColor = 'yellow'; });") // Highlight the text
+    ->assertSee($textToFind); // Check if the text is present and highlighted
 ```
 
-If you can provide more details about the specific HTML structure and what you are trying to interact with, I can offer more targeted assistance.
+In the above code, we first navigate to the page and then use JavaScript to highlight all elements containing the text (in this case, the current year). We set the background color to yellow, but you can adjust the styling as needed. After highlighting the text, we use `assertSee` to verify that the text is present on the page. If it's found and highlighted, the assertion will pass.
